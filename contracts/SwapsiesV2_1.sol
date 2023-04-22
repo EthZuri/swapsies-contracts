30ef164d85feaa18e488d39fab9daf6e18a446b5
iorulezz <iorulezz@gmail.com>
2023-04-22 19:06:00 +0200

    removed unnec imports

--- contracts/SwapsiesV2_1.sol
+++ contracts/SwapsiesV2_1.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "hardhat/console.sol";
 
 // With bundles
 contract SwapsiesV2_1 {
